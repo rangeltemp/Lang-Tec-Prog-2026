48c57ac9eaa280d136fc228e7ec53f9c9e0c6560
rangeltemp <pedroantoniorangel1709@gmail.com>
2026-08-05 22:19:27 -0300

    Add files via upload

---
+++ calculadora.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	
+	float a,b,c;
+	
+	a = 7;
+	b = 8;
+	c = 9;
+	
+	float media;
+	
+	media = (a + b + c) / 3;
+	
+	printf(" a média é %f", media);
+	
+	return 0;
+	
+}
